999a5c0a3c37c03b56832c6e3aa20680e89855ab
Eric Winbigler <uzof99@gmail.com>
2019-07-11 13:26:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,9 @@
-# require modules here
+# require modules here  
+require "yaml"
 
 def load_library
   # code goes here
-  require "yaml"
+ 
   load_library = YAML.load_file("./lib/emoticons.yml")
 end
 
